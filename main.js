750e1e7f27f60a0a7b2b1d26798db67837359b68
HEFACARI <hectorcaez24@gmail.com>
2023-03-01 11:38:49 -0500

    mi primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.menuBTN').addEventListener('click', () => {
+    document.querySelector('.navMenu').classList.toggle('show');
+}); 
+
+ScrollReveal().reveal('.showCase');
+ScrollReveal().reveal('.newsCards',{delay:500});
+ScrollReveal().reveal('.cardsBannerOne',{delay:500});
+ScrollReveal().reveal('.cardsBannerTwo',{delay:500});
+
+
+
